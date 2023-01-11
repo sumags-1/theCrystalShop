f8b4ae0e8471ae085484c3f3162861323b9e9d09
sumags <sumagowrishankar@gmail.com>
2023-01-11 10:11:02 -0800

    more skeleton work  - 3  done

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+// +-+-+-+-+-+-+-+-+-+-+-+-+
+// |D|E|P|E|N|D|E|N|C|I|E|S|
+// +-+-+-+-+-+-+-+-+-+-+-+-+
+// access node packages
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const passport = require('./config/passport')()
+require("dotenv").config()
+// access models
+const db = require("./models")
+// access controllers
+
+
+
+// +-+-+-+-+-+-+-+-+-+-+
+// |M|I|D|D|L|E|W|A|R|E|
+// +-+-+-+-+-+-+-+-+-+-+
+
+// cors allows our frontend to communicate with the backend
+app.use(cors())
+// body parser: used for POST/PUT/PATCH routes: this will take incoming strings from the request body that are url encoded and parse them into an object that can be accessed in the request parameter as a property called body (req.body).
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(passport.initialize())
+
+// Use controllers for all other routes
+
+// +-+-+-+-+-+-+-+-+
+// |L|I|S|T|E|N|E|R|
+// +-+-+-+-+-+-+-+-+
+// `app.listen()` binds and listens for the connections on the specified host and port
+app.listen(process.env.PORT, () => {
+    console.log(`App is running at localhost:${process.env.PORT}`)
+})
