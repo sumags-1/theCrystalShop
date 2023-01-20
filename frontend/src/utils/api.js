5808712feda3a28b7ded39461b4435f39a9cf84f
sumags <sumagowrishankar@gmail.com>
2023-01-20 14:37:06 -0800

    added APIURL to env file and axios requests

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 // create user
 export async function createUser(formData) {
-    const { data } = await axios.post('signup', formData)
+    const { data } = await axios.post(`${process.env.APIURL}/user/signup`, formData)
     return data
 }
 
 // log in to user account
 export async function loginToAccount(formData) {
-    const { data } = await axios.post('login', formData)
+    const { data } = await axios.post(`${process.env.APIURL}/user/login`, formData)
     return data
 }
 
@@ -27,43 +27,42 @@ export async function loginToAccount(formData) {
 
 //crystalhome index route
 export async function getIndexRoute() {
-    const { data } = await axios.get('crystal')
+    const { data } = await axios.get(`${process.env.APIURL}/crystal`)
     return data
 }
 
 //add a crystal - admin function
 export async function createCrystal(formData) {
-    const crystalData = await axios.post('crystal/', formData)
+    const crystalData = await axios.post(`${process.env.APIURL}/crystal/`, formData)
     return crystalData.data
 }
 
 //show a crystal
 export async function showCrystal(id) {
-    const crystalData = await axios.get(`crystal/${id}`)
+    const crystalData = await axios.get(`${process.env.APIURL}/crystal/${id}`)
     return crystalData.data
 }
 
 //edit a crystal
 export async function editCrystal(id) {
-    const editedCrystalForm = await axios.get(`crystal/${id}`)
+    const editedCrystalForm = await axios.get(`${process.env.APIURL}/crystal/${id}`)
     return editedCrystalForm.data
 }
 
 //update a crystal
 export async function updateCrystal(id, formState) {
-    const updatedCrystalForm = await axios.put(`https://new-beginnings-app.herokuapp.com/crystal/${id}`, formState)
+    const updatedCrystalForm = await axios.put(`${process.env.APIURL}/crystal/${id}`, formState)
     return updatedCrystalForm.data
 }
 
 //delete a crystal
 export async function deleteCrystal(id) {
-    const deletedCrystal = await axios.delete(`${id}`)
-
+    const deletedCrystal = await axios.delete(`${process.env.APIURL}/crystal/${id}`)
     return console.log('crystal entry deleted!');
 }
 
 // create review
 export async function createReview(id, formState) {
-    const createdReview = await axios.put(`crystal/${id}/review`, formState)
+    const createdReview = await axios.put(`${process.env.APIURL}/crystal/${id}/review`, formState)
     return createdReview.data
 };
